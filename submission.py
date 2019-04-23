3cdccc8d6a60381e078f8edc40d827fa5faf3924
leo <leo.hoare@live.com>
2019-04-23 22:46:43 +1000

    changes

--- submission.py
+++ submission.py
@@ -1,27 +1,14 @@
 import pandas as pd
 import numpy as np
-""" 
-parses file into pandas array 
-returns a pandas array
-transition probability of row into column
-NEED TO MAKE A METHOD TO SUPPORT BIG DATA
-"""
-
-
 
 def parse_state_file(file):
     file = open(file)
     lines = [line.split() for line in file]
     col_count = int(lines[0][0])
     cols = [lines[i][0] for i in range(1, col_count+1)]
-    # data = [[1 for _ in range(col_count-1)] for _ in range(col_count-2)]
-    # begin = np.ones(col_count)
     data = [[1 for _ in range(col_count-2)]+[0,1] \
              for _ in range(col_count-1)] + \
             [[0 for _ in range(col_count)]]
-    
-    
-    
     for i in range(col_count+1, len(lines)):
         x,y,score = lines[i]
         data[int(x)][int(y)] = int(score) + 1
@@ -34,13 +21,14 @@ def parse_symbol_file(file,state_cols):
     file = open(file)
     lines = [line.split() for line in file]
     col_count = int(lines[0][0])
-    cols = [lines[i][0] for i in range(1, col_count+1)]
-    out = np.ones((len(state_cols),col_count))
+    cols = [lines[i][0] for i in range(1, col_count+1)]+['UNK']
+    out = np.ones((len(state_cols),col_count+1))
+    # out[]
     for row in lines[(col_count+1):]:
-        out[int(row[0])][int(row[1])] = int(row[2])+1
+        out[int(row[0]),int(row[1])] = int(row[2])+1
     for index in range(out.shape[0]):
         total = sum(out[index])
-        prob = lambda t: t/(total+1)
+        prob = lambda t: t/(total)
         func = np.vectorize(prob)
         out[index]  = func(out[index])
     return cols, out
@@ -63,94 +51,69 @@ def advanced_decoding(State_File, Symbol_File, Query_File): # do not change the 
     pass # Replace this line with your implementation...
 
 
-def findVect(matrix, cols, symbls, value):
+def findVect(matrix,symbls, value):
     try: 
         return matrix[:,symbls.index(value)]
     except ValueError:
-        return np.log10([1/(len(cols)+len(symbls)+1)])
-
-
+        return matrix[:,symbls.index('UNK')]
 
 if __name__=="__main__":
     
     state_cols, state_matrix = parse_state_file('./dev_set/State_File')
     symbol_cols, symbol_matrix = parse_symbol_file("./dev_set/Symbol_File",state_cols)
-    # y = np.array(['Unit5',',' ,'10', '-' ,'14' ,'Munt', 'St',',' ,'Bayswater',',' ,'WA', '6053'])
 
+    '''
+    Unsure of where issues are arising, possibly around dealing with beg / end cases
+    current method:
+    Create transition from states
+        N.N matrix
+        End has 0 probability away from it
+        Begin has 0 probability to it
+        rest are calculated as ones and value overwritten if in state file (+1)
+        then probabilities calculated from this
+    create emission from symbols
+        N.K matrix
+        all initalised to zeros with column added for unknown
+        values overwritten from symbol file (+1) 
+        calculated probabilites
+    1. find initial probabilities from the transition matrix (BEGIN state)
+        please confirm?
+    2. iterate through each observation in y 
+        most of logic is here
+        first line T1[:, i - 1] * state_matrix.T * findVect(symbol_matrix,symbol_cols,y[i]).T
+            times last states * prob next state given state * emission prob of observed
+            find max of this for each state // prob of that state
+            to matrix running probabilities
+        T2 -> this displays index of path. 
+            similar
+            last states probs * next states -> array of N by N, then for each N find max prob index.
+    3. first find argmax of last state 
+
+    '''
+
+    # y = np.array(['Unit5',',' ,'10', '-' ,'14' ,'Munt', 'St',',' ,'Bayswater',',' ,'WA', '6053'])
     # y = np.array(['RMB','526',',','Borden',',','WA','6338'])
     # y = np.array(['Dumbuoy', 'Rd',',' ,'Warracknabeal',',' ,'VIC' ,'3393'])
-    # y = np.array(['MBF', '101a' ,'Pyke' ,'Rd',',', 'Mooroopna',',' ,'VIC' ,'3629'])
-    # K = state_matrix.shape[0]
-    # Pi = state_matrix[state_cols.index("BEGIN")]
-    # T = len(y)
-    # T1 = np.empty((K, T), 'd')
-    # T2 = np.empty((K, T), 'B')
-    # T1[:, 0] = Pi * findVect(symbol_matrix,state_cols,symbol_cols,y[0])
-    # T2[:, 0] = state_cols.index("BEGIN")
-    # for i in range(1, T):
-    #     T1[:, i] = np.max(T1[:, i - 1] * state_matrix.T * findVect(symbol_matrix,state_cols,symbol_cols,y[i]).T, 1)
-    #     T2[:, i] = np.argmax(T1[:, i - 1] * state_matrix.T, 1)
-    # x = np.empty(T+1, 'B')
-    # x[-1] = np.argmax(T1[:, T - 1])
-    # for i in reversed(range(1,T)):
-    #     x[i] = T2[x[i], i]
+    y = np.array(['BEGIN','MBF', '101a' ,'Pyke' ,'Rd',',', 'Mooroopna',',' ,'VIC' ,'3629','END'])
     
-    with np.errstate(divide='ignore'):
-        transition = np.log10(state_matrix)
-        emission = np.log10(symbol_matrix)
-
-    prev = []
-
-    events = ['MBF' ,'101a','Pyke' ,'Rd',',', 'Mooroopna',',' ,'VIC' ,'3629']
-    # events = ['RMB','526',',','Borden',',','WA','6338','END']
+    K = state_matrix.shape[0]
+    Pi = state_matrix[state_cols.index("BEGIN")]
+    T = len(y)
+    T1 = np.empty((K, T), 'd')
+    T2 = np.empty((K, T), 'B')
+    # case for start case (force state to begin)
+    T1[:, 0] = Pi 
+    T2[:, 0] = state_cols.index('BEGIN')
+    # iterate through all other cases
+    for i in range(1, T-1):
+        T1[:, i] = np.max(T1[:, i - 1] * state_matrix.T * findVect(symbol_matrix,symbol_cols,y[i]).T, 1)
+        T2[:, i] = np.argmax(T1[:, i - 1] * state_matrix.T, 1)
+    # case for end case (force state to end)
+    T1[:,T-1]=T1[:, i - 1] * state_matrix[:,state_cols.index('END')]
+    T2[:,T-1]=state_cols.index('END')
     
-    # logprob = initial + findVect(emission,state_cols,symbol_cols,next(events))
-    # emission[:, next(events)]
-    p=logprob=transition[state_cols.index("BEGIN")] + findVect(emission,state_cols,symbol_cols,events[0])
-    # prev.append([state_cols.index("BEGIN") for _ in range(transition.shape[0])])
-    # print(np.argmin(p))
-    for event in events:
-        p = logprob[:, np.newaxis] + transition + findVect(emission,state_cols,symbol_cols,event)
-        # if event=='MBF':
-        #     print(p)
-        logprob = np.max(p, axis=0)
-        # if event != 'END'
-        prev.append(np.argmax(p, axis=0))
-    
-    
-    print(prev)
-    best_state = np.argmax(logprob)
-    state = best_state
-    path = [state]
-    for p in reversed(prev):
-        state = p[state]
-        path.append(state)
-    
-    print(path)
-    # print(logprob(best_state]))
-    #     print(i)
-    #     x[i] = T2[x[i-1],i-1]
-    
-    # x[-2] = T2[x[-1],8]
-    # x[-3] = T2[x[-2],7]
-    # x[-4] = T2[x[-3],6]
-    # x[-5] = T2[x[-4],5]
-    # for i in reversed(range(2, T+1)):
-    #     print(f"HELLO {T2[x[i-1]][i-1]} {x[i-1]} {i-1}")
-        # x[i] = T2[x[i-1]][i-1]
-    
-    # T1[:, T-1] = np.max(T1[:, T-2]*state_matrix.T * findVect(symbol_matrix,state_cols,symbol_cols,y[T-1]).T, 1)
-    # T1[:, T-1] = np.max(T1[:, T-1] * state_matrix.T * findVect(symbol_matrix,state_cols,symbol_cols,y[T]).T, 1)
-    # T2[:, T+1] = np.argmax(T1[:, T+1 - 1] * state_matrix.T, 1)
-
-    # x = np.empty(T, 'B')
-    # x[-1] = np.argmax(T1[:, T - 1])
-    # for i in reversed(range(1, T)):
-    #     x[i - 1] = T2[x[i], i]
-    # x=np.append([state_cols.index("BEGIN")], x)
-    # for row in T1:
-    #     print(row)
-    # print(T1)
-    # print(T2)
-    # print(x)
-    
+    x = np.empty(T, 'B')
+    x[-1] = 25
+    for i in reversed(range(1, T)):
+        x[i - 1] = T2[x[i], i-1]
+    print(x)
